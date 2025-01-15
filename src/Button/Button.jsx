26cae00ec8115c42ae8bd40fd589d8c6c3293631
Adriano Salles <adrianogsallesf@gmail.com>
2025-01-15 14:09:23 -0300

    remove required props

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -8,6 +8,6 @@ export default function Button({ onClick, children }) {
 }
 
 Button.propTypes = {
-  onClick: PropTypes.func.isRequired,
-  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func,
+  children: PropTypes.node,
 };
